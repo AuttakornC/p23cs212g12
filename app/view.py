88a9ecaa010c66146e271481a8c4e9a778880f4c
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2023-11-13 00:50:17 +0700

    Fixed, Start Lab01

--- app/view.py
+++ app/view.py
@@ -2,4 +2,9 @@ from app import app
 
 @app.route("/")
 def home():
-    return "Hello world!"
+    return "auttakorn say : Hello world!"
+
+
+@app.route("/phonebook")
+def index():
+    return app.send_static_file("phonebook.html")
