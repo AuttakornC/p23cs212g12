d14e3465f3d6b587d8901ab38d4779a5c31758b4
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2023-11-13 13:13:29 +0700

    Fixing Title + API

--- app/hw_views.py
+++ app/hw_views.py
@@ -12,12 +12,9 @@ def api_weather():
     res = urlopen("https://api.waqi.info/feed/here/?token=7af3dd2f0ebfd5bda114fbbc3fb501edebf05c33")
     dict_data = json.load(res)["data"]
     return jsonify({
-        "city": dict_data["city"]["name"],
-        "data": {
-            "AQI": dict_data["aqi"],
-            "PM10": dict_data["iaqi"]["pm10"]["v"],
-            "PM2.5": dict_data["iaqi"]["pm25"]["v"],
-            "Temperature": dict_data["iaqi"]["t"]["v"],
-            "Time": dict_data["time"]["iso"]
-        }
+        "AQI": dict_data["aqi"],
+        "PM10": dict_data["iaqi"]["pm10"]["v"],
+        "PM2.5": dict_data["iaqi"]["pm25"]["v"],
+        "Temperature": dict_data["iaqi"]["t"]["v"],
+        "Time": dict_data["time"]["iso"]
     })
