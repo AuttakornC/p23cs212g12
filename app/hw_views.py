240b4ce00a4fe786b38dfa66eaabda56a5bfd750
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2023-11-28 17:29:16 +0700

    Finish HW03

--- app/hw_views.py
+++ app/hw_views.py
@@ -4,8 +4,10 @@
 
 import json
 from app import app
-from flask import jsonify
+from flask import jsonify, render_template
 from urllib.request import urlopen
+
+from datetime import datetime
 
 @app.route("/weather")
 def hw01_localweather():
@@ -22,3 +24,46 @@ def api_weather():
         "Temperature": dict_data["iaqi"]["t"]["v"],
         "Time": dict_data["time"]["iso"]
     })
+
+@app.route("/hw03/pm25/")
+def hw03_pm25():
+    res = urlopen("https://api.waqi.info/feed/ChiangMai/?token=7af3dd2f0ebfd5bda114fbbc3fb501edebf05c33")
+    dict_data = json.load(res)["data"]["forecast"]["daily"]["pm25"]
+    first_date = list(map(lambda x: int(x) ,dict_data[0]["day"].split("-")))
+    day = datetime(first_date[0], first_date[1], first_date[2]).weekday()
+    dict_data = (["-" for i in range((day+1)%7)]) + dict_data
+    week_split = []
+    week = []
+    before = "-"
+    for i in dict_data:
+        if i != "-":
+            if before == "-":
+                i["status"] = "-"
+            elif i["min"] == before:
+                i["status"] = "eq"
+            elif i["min"] > before:
+                i["status"] = "gt"
+            else:
+                i["status"] = "lt"
+            before = i["min"]
+
+            if i["min"] <= 50:
+                i["color"] = "good"
+            elif i["min"] <= 100:
+                i["color"] = "moderate"
+            elif i["min"] <= 150:
+                i["color"] = "unhealthy-sensitive"
+            elif i["min"] <= 200:
+                i["color"] = "unhealthy"
+            elif i["min"] <= 300:
+                i["color"] = "very-unhealthy"
+            else:
+                i["color"] = "hazardous"
+        week.append(i)
+        if len(week)==7:
+            week_split.append(week[:])
+            del week[:]
+    if len(week)!=7:
+        week = week+(["-"]*(7-len(week)))
+        week_split.append(week[:])
+    return render_template("lab03/hw03_pm25.html", datas=week_split, first_day=day)
