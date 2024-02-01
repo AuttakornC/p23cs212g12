0ef03d3dadc36598cc888caf90d4683f914ac1fc
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-02-01 12:12:23 +0700

    New Setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__, static_folder="client/static")
+app = Flask(__name__, static_folder="static", template_folder="template")
 app.url_map.strict_slashes = False
 app.config["DEBUG"] = True
 
 # serve client
-from app import client
-
-# api server
-from app.server import api
-app.register_blueprint(api, url_prefix="/api") # create route prefix to api path
+from app import route
--- app/client/__init__.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-# client serve
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>") # anypath that is not in server side will redirect to index.html only
-def serve(path):
-    # serve index.html only
-    return app.send_static_file("index.html")
