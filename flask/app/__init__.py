aa6fa33108965448c1c962b1f77914d50c1ea23e
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-02-04 18:00:09 +0700

    LAB10

---
+++ flask/app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from werkzeug.debug import DebuggedApplication
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, static_folder='static')
+app.url_map.strict_slashes = False
+
+
+app.jinja_options = app.jinja_options.copy()
+app.jinja_options.update({
+    'trim_blocks': True,
+    'lstrip_blocks': True
+})
+
+
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = \
+    'ce9262b39d8efd3757681a63ea7a6f97e4da7bcc10871a17'
+app.config['JSON_AS_ASCII'] = False
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+if app.debug:
+    app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+
+from app import views  # noqa
