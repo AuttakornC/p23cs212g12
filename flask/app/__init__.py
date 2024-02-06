7124b7763378428a9e5345f86d48e791a8a0c316
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-02-06 16:26:31 +0700

    Test route template & session

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from werkzeug.debug import DebuggedApplication
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='static', template_folder='template')
 app.url_map.strict_slashes = False
 
 app.jinja_options = app.jinja_options.copy()
