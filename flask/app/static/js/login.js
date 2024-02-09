8ef16c4351fd18426eb57867ee0896082612eee2
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-02-09 17:04:05 +0700

    add validate & fav

---
+++ flask/app/static/js/login.js
@@ -0,0 +1,70 @@
+
+const login_f = document.getElementById("login-form");
+login_f.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const form_object = {
+        email: e.target.email.value,
+        password: e.target.password.value
+    };
+    clearErr();
+    console.log(form_object);
+    const form_checked = formValidate(form_object);
+    if (form_checked[0]) {
+        // not pass
+        errHandle(form_checked[1]);
+    } else {
+        // pass
+        console.log("Pass");
+    }
+
+});
+
+// function for validate form
+function formValidate(data) {
+    let have_err = false;
+    let err_message = {};
+    
+    // email check
+    if (!data.email || data.email.split("@").length!==2 || !data.email.split("@")[1].includes(".") || data.email[data.email.length-1]==".") {
+        have_err = true;
+        err_message["email"] = "email's invalid.";
+    }
+    // password check
+    if (!data.password || data.password.length < 8 || data.password.length > 20) {
+        have_err = true;
+        err_message["password"] = "password must be in range 8-20 charactors";
+    }
+
+    return [have_err, err_message];
+}
+
+// function for err show
+function errHandle(data) {
+    for (const key in data) {
+        console.log(key);
+        const element = document.getElementById(key);
+
+        // console.log(element.children);
+        const input = element.children[1];
+        const err_message = document.createElement("p");
+        err_message.classList.add("err_text");
+        err_message.innerHTML = data[key];
+        input.classList.add("err");
+        element.appendChild(err_message);
+    }
+}
+
+// function for clear err
+function clearErr() {
+    const err_text_tags = document.getElementsByClassName("err_text");
+    for (const err_text_tag of err_text_tags) {
+        err_text_tag.parentNode.removeChild(err_text_tag);
+    }
+
+    const err_ps = document.getElementsByClassName("err");
+    for (const err_p of err_ps) {
+        err_p.classList.remove("err");
+    }
+    
+}
