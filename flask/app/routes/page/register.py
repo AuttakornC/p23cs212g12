313b77678b969f977e13c31b1eb8a682bfaa482b
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-02-17 19:23:21 +0700

    Rebase

--- flask/app/routes/page/register.py
+++
@@ -1,9 +0,0 @@
-# lib from py
-from flask import render_template
-
-# my lib
-from app.routes.page import main
-
-@main.route("/register")
-def register():
-    return render_template("register.html")
