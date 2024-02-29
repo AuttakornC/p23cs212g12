1c630f1ea60d0bc2699a548cc9e497b34920427d
meaww2 <childenchaos@gmail.com>
2024-02-29 14:13:55 +0700

    finish UI mobile Home page

--- flask/app/static/js/home.js
+++ flask/app/static/js/home.js
@@ -25,32 +25,41 @@ class Deck {
         const deckContainner = document.getElementsByClassName('deck-bg');
         data["data"].forEach(element => {
             console.log(element);
-            deckContainner[0].innerHTML += `<div class="box">
-            <div class="profile">
-                <img class="profile-icon" src="/static/image/profile-icon.png" alt="">
-                <span class="people">${element.username}</span>
-            </div>
-            <div class="description"><h4 id="nameD" class="deckName">${element.name}</h4><h5 class="cardNum">${element.len_card} Cards</h5></div>
-            <button class="button">Preview</button>
-        </div>`
+            deckContainner[0].innerHTML += `
+            <div class="box">
+                <div class="profile">
+                    <img class="profile-icon" src="/static/image/profile-icon.png" alt="">
+                    <span class="people"><h3>${element.username}</h3></span>
+                </div>
+                <div class="description">
+                    <h3 id="nameD" class="deckName">${element.name}</h3>
+                    <span class="cardNum">${element.len_card} Cards</span>
+                </div>
+                <div class="window-size">
+                    <button id='edit' onclick="onChange('/edit');" type="button">
+                        icon
+                    </button>
+                    <button id='play' onclick="onChange('/play');" type="button">
+                        icon
+                    </button>
+                    <button id='delete' onclick="delete();" type="button">
+                        icon
+                    </button>
+                </div>
+
+            </div>`
           
         });
         
     }
 }
 
-// add Deck
-// const deckBg = document.getElementsByClassName('deck-bg');
-// function onAdd()
-// {
-//     if (deckBg.lastElementChild) {
-//         const last_number = parseInt(deckBg.lastElementChild.getAttribute("order_"));
-//         if (!isNaN(last_number)) {
-//             number_input = last_number+1;
-//         }
-//     }
-// }
+function onChange(redirectUrl)
+{
+    // const redirectUrl = '/create_deck';
+    window.location.href = redirectUrl;
+}
 
 const handler = new Deck();
-// handler.getDeck();
-console.log('Aaaaaaa');
+
+console.log($('.title-deck'))
