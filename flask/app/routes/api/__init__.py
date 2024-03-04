7c339b34caa5ba1aa5c84800b0b0da69588ebb82
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-03-04 14:43:55 +0700

    Fixed

--- flask/app/routes/api/__init__.py
+++ flask/app/routes/api/__init__.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
 api = Blueprint("api", __name__, url_prefix="/api")
 
-from app.routes.api import deck, tag, suggest, explore, play
-
+from app.routes.api import deck, tag, suggest, explore, play, profile
