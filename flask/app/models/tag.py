a601860768be150a525b0dc5400089ec45689b41
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2024-03-04 20:19:22 +0700

    Fix DB

--- flask/app/models/tag.py
+++ flask/app/models/tag.py
@@ -10,12 +10,14 @@ class Tag(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    create_at = db.Column(db.DateTime())
     is_deleted = db.Column(db.Boolean, default=False)
     delete_at = db.Column(db.DateTime())
 
     def __init__(self, name):
         self.name = name
         self.is_deleted = False
+        self.create_at = datetime.now(timezone.utc)
         self.delete_at = None
     
     def update(self, name):
